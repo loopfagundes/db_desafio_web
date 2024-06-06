d160a59cffc162b552f86c9e4e66a565fdd15f34
Luciano Oliveira Fagundes <luciano.fagundes@dbserver.com.br>
2024-06-06 15:42:10 -0300

    criou uma classe testcase e executou com sucesso.

--- src/test/java/app/netlify/bugbank/testcases/BugBankTestCase.java
+++ src/test/java/app/netlify/bugbank/testcases/BugBankTestCase.java
@@ -8,7 +8,11 @@ import org.testng.annotations.Test;
 
 public class BugBankTestCase extends BaseTest {
 
-    @Test
+    @Test(
+            description = "Criar duas contas com saldo e realizar uma transferência valor para outra.",
+            priority = 1,
+            groups = {"web"}
+    )
     public void BugBank() {
         WebDriver driver = DriverManager.getDriver();
         driver.get(Property.get("url"));
