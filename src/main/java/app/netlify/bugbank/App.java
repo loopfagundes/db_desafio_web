d8ee0d9df92bc4138caa3851462d0de262b1be5f
Luciano Oliveira Fagundes <luciano.fagundes@dbserver.com.br>
2024-06-06 14:41:09 -0300

    iniciou um projeto

---
+++ src/main/java/app/netlify/bugbank/App.java
@@ -0,0 +1,13 @@
+package app.netlify.bugbank;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
