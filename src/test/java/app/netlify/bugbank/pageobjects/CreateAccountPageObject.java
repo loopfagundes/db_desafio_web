4717ca7e66ab549080598fc667770c657f62d692
Luciano Oliveira Fagundes <luciano.fagundes@dbserver.com.br>
2024-06-07 16:21:44 -0300

    clean code

--- src/test/java/app/netlify/bugbank/pageobjects/CreateAccountPageObject.java
+++ src/test/java/app/netlify/bugbank/pageobjects/CreateAccountPageObject.java
@@ -15,7 +15,7 @@ public class CreateAccountPageObject {
         return new Element(this.driver, By.xpath("//*[@id=\"__next\"]/div/div[2]/div/div[1]/form/div[3]/button[2]"));
     }
 
-    public Element registerEmailTextField() {                             //*[@id="__next"]/div/div[2]/div/div[1]/form/div[1]/input
+    public Element registerEmailTextField() {
         return new Element(this.driver, By.xpath("//*[@id=\"__next\"]/div/div[2]/div/div[2]/form/div[2]/input"));
     }
 
