12f236e779d2ecfa0e6bad0d98539bcc746a5472
loopfagundes <lof.fagundes@gmail.com>
2024-07-05 16:05:23 -0300

    melhoras estrutura, exculi um metodo repetido.

--- src/test/java/app/netlify/bugbank/steps/AccountScreenStep.java
+++ src/test/java/app/netlify/bugbank/steps/AccountScreenStep.java
@@ -46,8 +46,8 @@ public class AccountScreenStep {
     private AccountScreenStep makeTransfer(String description) throws IOException {
         Report.log(Status.INFO, "Pagina da minha conta e realizar a transferência");
         accountScreenPageObject.transferButton().click();
-        accountScreenPageObject.justNumberAccountTextField().sendKeys(justNumber("secondUser", "justNumber"));
-        accountScreenPageObject.accountDigitTextField().sendKeys(accountDigit("secondUser", "accountDigit"));
+        accountScreenPageObject.justNumberAccountTextField().sendKeys(getAccountNumber("secondUser", "justNumber"));
+        accountScreenPageObject.accountDigitTextField().sendKeys(getAccountNumber("secondUser", "accountDigit"));
         RecorderSet.fakeValue(accountScreenPageObject.transferAmountTextField(), "firstUser", "value");
         accountScreenPageObject.descriptionTextField().sendKeys(description);
         accountScreenPageObject.transferNowButton().click();
