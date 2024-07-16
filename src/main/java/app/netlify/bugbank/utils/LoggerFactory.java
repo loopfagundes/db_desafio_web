eff8c46871e33bffcb23ae0ac8ee93d2102210f5
loopfagundes <lof.fagundes@gmail.com>
2024-07-16 17:04:20 -0300

    clean code

--- src/main/java/app/netlify/bugbank/utils/LoggerFactory.java
+++ src/main/java/app/netlify/bugbank/utils/LoggerFactory.java
@@ -6,10 +6,6 @@ import java.util.logging.Logger;
 public class LoggerFactory {
     private static final Logger logger = Logger.getLogger(LoggerFactory.class.getName());
 
-    public static void log_INFO(String msg) {
-        logger.log(Level.INFO, msg);
-    }
-
     public static void log_WARNING(String msg) {
         logger.log(Level.WARNING, msg);
     }
